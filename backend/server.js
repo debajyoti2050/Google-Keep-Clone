019aca50598927bf472e9b681eb097057a5b27fe
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-10-10 00:05:23 +0530

    backend-registration-api

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const notes= require('./data/notes')
 const dotenv = require('dotenv')
 const connectDB= require("./config/db")
+const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
 
 const app = express();
 dotenv.config();
 connectDB();
+
+app.use(express.json())
 
 
 app.get("/",(req,res)=>{
@@ -21,6 +25,10 @@ app.get("/api/notes/:id",(req,res)=>{
     res.json(note)
 })
 
+app.use("/api/users",userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server Stared on port ${PORT}`));
